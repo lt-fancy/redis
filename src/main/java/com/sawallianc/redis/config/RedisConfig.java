b844cb2584b4e319dda634999e259887aae7905b
fingertap <fingertap588@gmail.com>
2018-08-04 15:55:09 +0800

    更改序列化为string

--- src/main/java/com/sawallianc/redis/config/RedisConfig.java
+++ src/main/java/com/sawallianc/redis/config/RedisConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 /**
  * Created by fingertap on 2017/6/8.
@@ -20,8 +21,7 @@ public class RedisConfig {
     public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
         RedisTemplate<String,Object> template = new RedisTemplate();
         template.setConnectionFactory(factory);
-//        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         template.setKeySerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
